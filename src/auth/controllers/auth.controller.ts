622b2b6abf28c95d99871ee966baf5c356d8a89d
SubDev <bilomarin@gmail.com>
2024-11-11 10:55:45 -0300

    Dependências atualizadas, documentação da API do Swagger adicionada e controladores e DTOs modificados para incluir anotações do Swagger

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,12 +1,17 @@
-// auth.controller.ts
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from '../auth.service';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'Realizar login de um usuário' })
+  @ApiBody({ description: 'Dados de login', type: Object })
+  @ApiResponse({ status: 201, description: 'Login realizado com sucesso' })
+  @ApiResponse({ status: 401, description: 'Credenciais inválidas' })
   async login(@Request() req) {
     return this.authService.login(req.body);
   }
